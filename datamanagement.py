0cd74d608a3beff130e130e1ddc09a29ae5020f8
MarkBringNielsen <markbringnielsen@gmail.com>
2020-10-20 14:38:34 +0200

    Fixed import and create shifts

--- datamanagement.py
+++ datamanagement.py
@@ -1,18 +1,25 @@
 import json
+from models.shift import Shift
+from models.employee import Employee
+from datetime import datetime, timedelta
 
 def load():
     with open('smaller_schedule.json') as data:
         data = json.load(data)
 
 
+        shifts = []
+        employees = []
 
 
-        
+
+        shifts = [Shift(day=s['day'], start=s['startTime'],end=s['endTime'], location=s['location']) for s in data['shifts']]
+        #for json_employee in data['employees']:
 
 
 
-        shifts = data['shifts']
-        employees = data['employees']
 
         return shifts, employees
 
+if __name__ == "__main__":
+    print(load())
